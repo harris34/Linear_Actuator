c3a3ea615517c805f37d8ca581bdf3a8cc476020
Jacob Harris <Harris34@students.rowan.edu>
2015-04-02 09:09:48 -0400

    misc. BS files

---
+++ Debug/main.pp
@@ -0,0 +1,15 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: D:/HDD\ programs/code\ composer/ccsv6/ccs_base/msp430/include/msp430.h
+main.obj: D:/HDD\ programs/code\ composer/ccsv6/ccs_base/msp430/include/msp430f5529.h
+main.obj: D:/HDD\ programs/code\ composer/ccsv6/ccs_base/msp430/include/in430.h
+main.obj: D:/HDD\ programs/code\ composer/ccsv6/tools/compiler/ti-cgt-msp430_4.4.4/include/intrinsics.h
+main.obj: D:/HDD\ programs/code\ composer/ccsv6/tools/compiler/ti-cgt-msp430_4.4.4/include/intrinsics_legacy_undefs.h
+
+../main.c: 
+D:/HDD\ programs/code\ composer/ccsv6/ccs_base/msp430/include/msp430.h: 
+D:/HDD\ programs/code\ composer/ccsv6/ccs_base/msp430/include/msp430f5529.h: 
+D:/HDD\ programs/code\ composer/ccsv6/ccs_base/msp430/include/in430.h: 
+D:/HDD\ programs/code\ composer/ccsv6/tools/compiler/ti-cgt-msp430_4.4.4/include/intrinsics.h: 
+D:/HDD\ programs/code\ composer/ccsv6/tools/compiler/ti-cgt-msp430_4.4.4/include/intrinsics_legacy_undefs.h: 
